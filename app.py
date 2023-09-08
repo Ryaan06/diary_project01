ec5f37961331df4ac19002e5e809d0f0de72db5b
Ryaan06 <aryanakusuma31@gmail.com>
2023-09-08 14:43:50 +0700

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from datetime import datetime
 
 connection_string = "mongodb+srv://aryana:sparta@cluster0.6jg1iti.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient (connection_string)
@@ -20,11 +21,22 @@ def save_diary():
     title_receive = request.form["title_give"]
     content_receive = request.form["content_give"]
 
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
     file = request.files["file_give"]
-    save_to = 'static/mypicture.jpg'
-    file.save(save_to)
+    extension = file.filename.split('.')[-1]
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+
+    profile = request.files["profile_give"]
+    extension = profile.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}'
+    profile.save(profilename)
 
     doc = {
+        'file':filename,
+        'profile':profilename,
         'title':title_receive,
         'content':content_receive
     }
